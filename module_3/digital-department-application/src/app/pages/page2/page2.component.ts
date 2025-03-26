d944399fda54b89d2a8f8662147b205409a64284
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2025-03-26 17:50:15 +0400

    Did tasts 4-5

--- module_3/digital-department-application/src/app/pages/page2/page2.component.ts
+++ module_3/digital-department-application/src/app/pages/page2/page2.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page2',
@@ -9,5 +9,9 @@ import { RouterModule } from '@angular/router';
   styleUrl: './page2.component.scss'
 })
 export class Page2Component {
+  constructor(private router: Router) {}
 
+  goMain(){
+    this.router.navigateByUrl("/")
+  }
 }
