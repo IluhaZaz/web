6ab6681bf66a98f0fe366d247a6fd9fac14b4fce
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2025-03-26 19:17:46 +0400

    Finished 3rd lab Add a few styles

--- module_3/digital-department-application/src/app/app.component.ts
+++ module_3/digital-department-application/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { ItalicDirective } from './core/directive/italic.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, RouterOutlet, RouterModule],
+  imports: [FormsModule, RouterOutlet, RouterModule, ItalicDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
