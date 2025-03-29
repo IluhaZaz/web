856584e84d220a103f968851e9af866af04e0927
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2025-03-29 23:23:03 +0400

    Finished 6th lab Defined styles

--- module_3/task-tracker/src/app/pages/tasks/tasks.component.ts
+++ module_3/task-tracker/src/app/pages/tasks/tasks.component.ts
@@ -51,14 +51,4 @@ export class TasksComponent implements OnInit {
     }
     return this.tasks.filter(task => task.status === this.statusFilter);
   }
-
-  getStatusClasses(status: Status): string {
-    switch (status) {
-      case 'open': return 'bg-blue-100 text-blue-800';
-      case 'in-progress': return 'bg-yellow-100 text-yellow-800';
-      case 'done': return 'bg-green-100 text-green-800';
-      case 'closed': return 'bg-gray-100 text-gray-800';
-      default: return '';
-    }
-  }
 }
